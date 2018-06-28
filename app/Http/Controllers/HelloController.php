2c36a3c61bed23ee1b29f029d6e9a2e88264d7e2
岩岡 顕夫 <>
2018-06-28 17:49:39 +0900

    MVCのビューとコントローラーまで学習完了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,46 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
+use Validator;
 
-global $head,$style,$body,$end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-     margin:-40px 0px -50px 0px; }
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></head>';
+class HelloController extends Controller {
+  
+   public function index(Request $request) {
+       return view('hello.index', ['msg'=>'フォームを入力して下さい。']);
+   }
 
-function tag($tag,$txt){
-    return "<{$tag}>".$txt."</{$tag}>";
-}
-
-class HelloController extends Controller
-{
-    public function index() {
-       global $head,$style,$body,$end;
-       
-       $html = $head.tag('title','hello/Index').$style
-               .$body
-                .tag('h1','Index').tag('p','this is Index page')
-                .'<a href="/hello/other">go to Other page</a>'
-               .$end;
-
-        return $html;
-    }
-
-    public function other(){
-        global $head,$style,$body,$end;
-       
-        $html = $head.tag('title','hello/Other').$style
-               .$body
-                .tag('h1','Other').tag('p','this is Other page')
-               .$end;
-
-        return $html;
-
+   public function post(HelloRequest $request) {
+      return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
---
+++ app/Providers/HelloServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Validator;
+use App\Http\Validators\HelloValidator;
+
+
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Validator::extend(
+            'hello',
+            function( $attribute, $value, $parameters, $validator ) {
+             return $value % 2 == 0;
+            }
+        ); 
+
+        $validator = $this->app['validator'];
+        $validator->resolver(
+            function($translator,$data,$rules,$messages) {
+                return new HelloValidator($translator, $data, $rules, $messages);
+            }
+        );
+
+        View::composer(
+            'hello.index','App\Http\Composers\HelloComposer'
+        );
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
